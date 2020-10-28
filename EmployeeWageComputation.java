9e62f2d3203195b13b959401bdae1168e37bf42e
vismaya01 <kvvismaya98@gmail.com>
2020-10-28 13:20:36 +0530

    Refactored: Calculate monthly employee wag

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,33 +5,34 @@ public class EmployeeWageComputation {
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 100;
-	//Calculating daily hour of employee
-	public static int employeeDailyHour(){
+	//Calculating monthly wage of employee
+	public static void employeeMonthlyWage(){
 		//variables
-		int empHours=0, dailyWage=0;
+		int empHours=0;
+		int totalWorkingDays=0, totalEmpHours=0;
 		int empCheck=(int) Math.floor(Math.random() * 10) % 3;
-		switch(empCheck){
-			case IS_PART_TIME:
-				empHours=8;
-				break;
-			case IS_FULL_TIME:
-				empHours=4;
-				break;
-			default:
-				empHours=0;
+		while (totalEmpHours <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+                        totalWorkingDays++;
+			switch(empCheck){
+				case IS_PART_TIME:
+					empHours=8;
+					break;
+				case IS_FULL_TIME:
+					empHours=4;
+					break;
+				default:
+					empHours=0;
+			}
+		totalEmpHours += empHours;
 		}
-		return empHours;
+	System.out.println("total employee working hour - "+totalEmpHours);
+        System.out.println("total employee working days - "+totalWorkingDays);
+        int totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
+        System.out.println("Total employee wage - "+totalEmpWage);
 	}
+
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation");
-		int totalWorkingDays=0, totalEmpHours=0;
-		while (totalEmpHours <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
-			totalWorkingDays++;
-			totalEmpHours += employeeDailyHour();
-		}
-		System.out.println("total employee working hour - "+totalEmpHours);
-		System.out.println("total employee working days - "+totalWorkingDays);
-		int totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
-		System.out.println("Total employee wage - "+totalEmpWage);
+		employeeMonthlyWage();
 	}
 }
