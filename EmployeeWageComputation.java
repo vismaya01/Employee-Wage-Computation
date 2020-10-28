f9474551f4c07abd8724bba9ceac73359ef89ade
vismaya01 <kvvismaya98@gmail.com>
2020-10-28 13:48:56 +0530

    Modified: Compute Employee Wage for multiple company

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,18 +1,16 @@
+import java.util.Scanner;
 public class EmployeeWageComputation {
 	//constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
 
 	//Calculating monthly wage of employee
-	public static void employeeMonthlyWage(){
+	public static void employeeMonthlyWage(int wagePerHour,int workingDays,int workingHours){
 		//variables
 		int empHours=0;
 		int totalWorkingDays=0, totalEmpHours=0;
 		int empCheck=(int) Math.floor(Math.random() * 10) % 3;
-		while (totalEmpHours <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+		while (totalEmpHours <= workingHours && totalWorkingDays < workingDays){
                         totalWorkingDays++;
 			switch(empCheck){
 				case IS_PART_TIME:
@@ -28,12 +26,20 @@ public class EmployeeWageComputation {
 		}
 	System.out.println("total employee working hour - "+totalEmpHours);
         System.out.println("total employee working days - "+totalWorkingDays);
-        int totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
+        int totalEmpWage = totalEmpHours * wagePerHour;
         System.out.println("Total employee wage - "+totalEmpWage);
 	}
 
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation");
-		employeeMonthlyWage();
+		//take user input for each company
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the wage per hour");
+		int wagePerHour=sc.nextInt();
+		System.out.println("enter the number of working days");
+		int numWorkingDays=sc.nextInt();
+		System.out.println("enter the number of working hour");
+		int numWorkingHours=sc.nextInt();
+		employeeMonthlyWage(wagePerHour,numWorkingDays,numWorkingHours);
 	}
 }
