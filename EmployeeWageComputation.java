34fe94fbc6a83718d234d7a90aa38bc26409b744
vismaya01 <kvvismaya98@gmail.com>
2020-10-28 11:27:33 +0530

    Added: Employee Attendance check

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,5 +1,15 @@
 public class EmployeeWageComputation {
+	public static void attendanceCheck(){
+		int empCheck=(int) Math.floor(Math.random() * 10) % 2;
+		if(empCheck==1){
+			System.out.println("Employee is Present");
+		}
+		else{
+			System.out.println("Employee is Absent");
+		}
+	}
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation");
+		attendanceCheck();
 	}
 }
