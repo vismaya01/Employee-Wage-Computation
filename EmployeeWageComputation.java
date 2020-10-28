e6dec130e7c9bf37b828f51984d4a249e8713d31
vismaya01 <kvvismaya98@gmail.com>
2020-10-28 12:41:09 +0530

    Added: Calculate monthly employee wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,8 +3,9 @@ public class EmployeeWageComputation {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
 	//Calculating daily wage of employee 
-	public static void employeeDailyWage(){
+	public static int employeeDailyHour(){
 		//variables
 		int empHours=0, dailyWage=0;
 		int empCheck=(int) Math.floor(Math.random() * 10) % 3;
@@ -18,11 +19,17 @@ public class EmployeeWageComputation {
 			default:
 				empHours=0;
 		}
-		dailyWage=empHours*EMP_RATE_PER_HOUR;
-		System.out.println("Employee daily wage - "+dailyWage);
+		return empHours;
 	}
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation");
-		employeeDailyWage();
+		int totalWorkingDays=0, totalEmpHours=0;
+		while (totalWorkingDays < NUM_OF_WORKING_DAYS){
+			totalWorkingDays++;
+			totalEmpHours += employeeDailyHour();
+		}
+		System.out.println("total employee working hour - "+totalEmpHours);
+		int totalEmpWage = totalEmpHours * EMP_RATE_PER_HOUR;
+		System.out.println("Total employee wage - "+totalEmpWage);
 	}
 }
