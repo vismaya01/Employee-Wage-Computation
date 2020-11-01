e8c89ddab998ead62a74bda5eb22c27db30d4f9a
vismaya01 <kvvismaya98@gmail.com>
2020-11-01 19:49:53 +0530

    Modified: Employee Wage for Multiple Company using Interface approach

---
+++ CompanyEmpWage.java
@@ -0,0 +1,23 @@
+public class CompanyEmpWage {
+
+        public String company;
+        public int wagePerHour;
+        public int workingDays;
+        public int workingHours;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(String company ,int wagePerHour,int workingDays,int workingHours){
+                this.company=company;
+                this.wagePerHour=wagePerHour;
+                this.workingDays=workingDays;
+                this.workingHours=workingHours;
+        }
+
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage=totalEmpWage;
+	}
+
+	public String toString() {
+		return "Total employee wage for comapny " +company+" is: "+totalEmpWage;
+	}
+}
--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,27 +4,31 @@ public class EmployeeWageComputation {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 
-	private String company;
-	private int wagePerHour;
-	private int workingDays;
-	private int workingHours;
-	private int totalEmpWage;
+	private int numOfCompany = 0;
+	private CompanyEmpWage[] companyEmpWageArray;
 
-	public EmployeeWageComputation(String company ,int wagePerHour,int workingDays,int workingHours){
-		this.company=company;
-		this.wagePerHour=wagePerHour;
-		this.workingDays=workingDays;
-		this.workingHours=workingHours;
+	public EmployeeWageComputation() {
+		companyEmpWageArray = new CompanyEmpWage[5];
 	}
 
+	private void addCompanyEmpWage(String company ,int wagePerHour,int workingDays,int workingHours){
+		companyEmpWageArray[numOfCompany] = new CompanyEmpWage(company,wagePerHour,workingDays,workingHours);
+		numOfCompany++;
+	}
+
+	private void computeEmpWage() {
+		for (int i =0; i < numOfCompany; i++) {
+			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
+			System.out.println(companyEmpWageArray[i]);
+		}
+	}
 	//Calculating monthly wage of employee
-	public void employeeMonthlyWage(){
+	private int computeEmpWage(CompanyEmpWage companyEmpWage){
 		//variables
-		int empHours=0;
-		int totalWorkingDays=0, totalEmpHours=0;
-		int empCheck=(int) Math.floor(Math.random() * 10) % 3;
-		while (totalEmpHours <= workingHours && totalWorkingDays < workingDays){
+		int empHours=0, totalWorkingDays=0, totalEmpHours=0;
+		while (totalEmpHours <= companyEmpWage.workingHours && totalWorkingDays < companyEmpWage.workingDays){
                         totalWorkingDays++;
+			int empCheck=(int) Math.floor(Math.random() * 10) % 3;
 			switch(empCheck){
 				case IS_PART_TIME:
 					empHours=8;
@@ -35,25 +39,17 @@ public class EmployeeWageComputation {
 				default:
 					empHours=0;
 			}
-		totalEmpHours += empHours;
+			totalEmpHours += empHours;
 		}
-	System.out.println("total employee working hour - "+totalEmpHours);
-        System.out.println("total employee working days - "+totalWorkingDays);
-        totalEmpWage = totalEmpHours * wagePerHour;
+		System.out.println("total employee working hour - "+totalEmpHours);
+        	System.out.println("total employee working days - "+totalWorkingDays);
+        	return totalEmpHours * companyEmpWage.wagePerHour;
 	}
-
-	public String toString(){
-        	return "Total employee wage for company : "+company+" is - "+totalEmpWage;
-	}
-
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation");
-		//take user input for each company
-		EmployeeWageComputation dmart = new EmployeeWageComputation("DMart",20,20,100);
-		EmployeeWageComputation reliance = new EmployeeWageComputation("Reliance",10,25,150);
-		dmart.employeeMonthlyWage();
-		System.out.println(dmart);
-		reliance.employeeMonthlyWage();
-		System.out.println(reliance);
+		EmployeeWageComputation empWage = new EmployeeWageComputation();
+		empWage.addCompanyEmpWage("Dmart",20,20,100);
+		empWage.addCompanyEmpWage("Reliance",10,25,150);
+		empWage.computeEmpWage();
 	}
 }
