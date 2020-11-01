0b796bdf6747f163cbe59d6c134a2b7f7afe0313
vismaya01 <kvvismaya98@gmail.com>
2020-11-01 18:40:48 +0530

    Modified : save the Total Wage for Each Company

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,8 +4,21 @@ public class EmployeeWageComputation {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 
+	private String company;
+	private int wagePerHour;
+	private int workingDays;
+	private int workingHours;
+	private int totalEmpWage;
+
+	public EmployeeWageComputation(String company ,int wagePerHour,int workingDays,int workingHours){
+		this.company=company;
+		this.wagePerHour=wagePerHour;
+		this.workingDays=workingDays;
+		this.workingHours=workingHours;
+	}
+
 	//Calculating monthly wage of employee
-	public static void employeeMonthlyWage(String company ,int wagePerHour,int workingDays,int workingHours){
+	public void employeeMonthlyWage(){
 		//variables
 		int empHours=0;
 		int totalWorkingDays=0, totalEmpHours=0;
@@ -26,14 +39,21 @@ public class EmployeeWageComputation {
 		}
 	System.out.println("total employee working hour - "+totalEmpHours);
         System.out.println("total employee working days - "+totalWorkingDays);
-        int totalEmpWage = totalEmpHours * wagePerHour;
-        System.out.println("Total employee wage for company : "+company+" is - "+totalEmpWage);
+        totalEmpWage = totalEmpHours * wagePerHour;
+	}
+
+	public String toString(){
+        	return "Total employee wage for company : "+company+" is - "+totalEmpWage;
 	}
 
 	public static void main(String[] args){
 		System.out.println("Welcome to Employee Wage Computation");
 		//take user input for each company
-		employeeMonthlyWage("DMart",20,20,100);
-		employeeMonthlyWage("Reliance",10,25,150);
+		EmployeeWageComputation dmart = new EmployeeWageComputation("DMart",20,20,100);
+		EmployeeWageComputation reliance = new EmployeeWageComputation("Reliance",10,25,150);
+		dmart.employeeMonthlyWage();
+		System.out.println(dmart);
+		reliance.employeeMonthlyWage();
+		System.out.println(reliance);
 	}
 }
